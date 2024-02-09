4d39198ddd8e49b55ce4352eadedcc7c04344d72
Frank Onesso <odhiambofrank1965@gmail.com>
2024-02-10 01:57:08 +0300

    added navbar details

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from "react";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <div>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top mt-2">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+              Frank Were
+            </a>
+          </div>
+          <div style={{ fontFamily: "Poppins" }}>
+            <a className="navbar-brand" href="#">
+              Home
+            </a>
+            <a className="navbar-brand" href="#">
+              Tech Stack
+            </a>
+            <a className="navbar-brand" href="#">
+              Community
+            </a>
+            <a className="navbar-brand" href="#">
+              Education
+            </a>
+            <a className="navbar-brand" href="#">
+              Experience
+            </a>
+            <a className="navbar-brand" href="#">
+              Projects
+            </a>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
 }
