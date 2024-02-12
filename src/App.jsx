7cf62f3e5b1f1013e36514a5fba307583b2391e1
Frank Onesso <odhiambofrank1965@gmail.com>
2024-02-13 00:57:19 +0300

    added nav style & hover

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,39 @@
 import React from "react";
-
+import "./style.css";
 
 export default function App() {
   return (
-    <>
+    <div>
       <div>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top mt-2">
-
+        <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top mt-2 radius">
           <div className="container-fluid">
-            <a className="navbar-brand" href="#">
-              Frank Were
-            </a>
+            <a className="navbar-brand name-1">Frank Were</a>
           </div>
 
-          <div>
-            <a className="navbar-brand" href="#">
+          <div className="Nav-style ">
+            <a className="navbar-brand name  " href="#">
               Home
             </a>
-            <a className="navbar-brand" href="#">
+            <a className="navbar-brand name " href="#">
               Tech Stack
             </a>
-            <a className="navbar-brand" href="#">
+            <a className="navbar-brand name " href="#">
               Community
             </a>
-            <a className="navbar-brand" href="#">
+            <a className="navbar-brand name " href="#">
               Education
             </a>
-            <a className="navbar-brand" href="#">
+            <a className="navbar-brand name " href="#">
               Experience
             </a>
-            <a className="navbar-brand" href="#">
+            <a className="navbar-brand name " href="#">
               Projects
             </a>
           </div>
         </nav>
       </div>
-    </>
+
+      <div></div>
+    </div>
   );
 }
