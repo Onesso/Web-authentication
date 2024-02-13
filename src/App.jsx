9b4a1f063069615232eea0d19a3c89bfbc4f72a3
Frank Onesso <odhiambofrank1965@gmail.com>
2024-02-14 02:48:50 +0300

    done with the nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -4,36 +4,62 @@ import "./style.css";
 export default function App() {
   return (
     <div>
+      <nav className="nav">
+        <div>
+          <p className="nav frank">FRANK WERE</p>
+        </div>
+
+        <div>
+          <ul className="stylo">
+            <li>
+              <a id="home-anchor" href="#">
+                Home
+              </a>
+            </li>
+            <li>
+              <a id="stack-anchor" href="#">
+                Tech-stack
+              </a>
+            </li>
+            <li>
+              {" "}
+              <a id="comm-anchor" href="#">
+                Community
+              </a>
+            </li>
+            <li>
+              <a id="Edu-anchor" href="#">
+                Education
+              </a>
+            </li>
+            <li>
+              <a id="Expe-anchor" href="#">
+                Experience
+              </a>
+            </li>
+            <li>
+              <a id="projo-anchor" href="#">
+                Projects
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
       <div>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top mt-2 radius">
-          <div className="container-fluid">
-            <a className="navbar-brand name-1">Frank Were</a>
-          </div>
-
-          <div className="Nav-style ">
-            <a className="navbar-brand name  " href="#">
-              Home
-            </a>
-            <a className="navbar-brand name " href="#">
-              Tech Stack
-            </a>
-            <a className="navbar-brand name " href="#">
-              Community
-            </a>
-            <a className="navbar-brand name " href="#">
-              Education
-            </a>
-            <a className="navbar-brand name " href="#">
-              Experience
-            </a>
-            <a className="navbar-brand name " href="#">
-              Projects
-            </a>
-          </div>
-        </nav>
+        <button className="toggle-bar">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-moon"
+            viewBox="0 0 16 16"
+          >
+            <path d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278M4.858 1.311A7.27 7.27 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.32 7.32 0 0 0 5.205-2.162q-.506.063-1.029.063c-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286" />
+          </svg>
+        </button>
       </div>
-
-      <div></div>
     </div>
   );
 }
