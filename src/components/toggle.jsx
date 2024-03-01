7f3b299da8f095123d84f7dd12ac1b45b2d4325f
Frank Onesso <odhiambofrank1965@gmail.com>
2024-03-01 23:33:21 +0300

    added the about me section

--- src/components/toggle.jsx
+++ src/components/toggle.jsx
@@ -2,7 +2,7 @@ import "./toggle.css";
 
 export const Toggle = ({ handleChange, isChecked }) => {
   return (
-    <div className="toggle-container">
+    <div className="toggle-container ">
       <input
         type="checkbox"
         id="check"
